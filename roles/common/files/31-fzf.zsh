429d34edfde19d225b9dd10f85b8b4521132a383
kazscape <3782734+kazscape@users.noreply.github.com>
2025-12-26 05:11:50 +0900

    Refactering of fzf, neovim and tmux

--- roles/common/files/31-fzf.zsh
+++ roles/common/files/31-fzf.zsh
@@ -31,7 +31,7 @@ _fzf_compgen_dir() {
   fd --type=d --hidden --exclude .git . "$1"
 }
 
-source ~/.config/fzf-git/fzf-git.sh
+source ~/.local/share/fzf-git/fzf-git.sh
 
 show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
 
